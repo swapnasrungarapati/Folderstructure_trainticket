5f327527bc7472372181abefbc14689c69fa83e8
swapnasrungarapati <swapnasnow@gmail.com>
2020-11-05 16:38:18 +0200

    train ticket

---
+++ src/main/java/com/ssrs/trainticket/TrainticketPlugin.java
@@ -0,0 +1,19 @@
+package com.ssrs.trainticket;
+
+import com.ssrs.framework.extend.plugin.AbstractPlugin;
+import com.ssrs.framework.extend.plugin.PluginException;
+
+/**
+ * @author ssrs
+ */
+public class TrainticketPlugin extends AbstractPlugin {
+    @Override
+    public void start() throws PluginException {
+
+    }
+
+    @Override
+    public void stop() throws PluginException {
+
+    }
+}
